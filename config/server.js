edc464aa7b2dd24403466ea91e0a8f68e3ddc6c7
Aymane DANIEL <aymane.daniel@ebp.com>
2022-05-12 17:31:50 +0200

    key

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "4CIqUOnXk1IZbNqzVoSauA==",
+      "bi0UBRTe7mzOnoPVb4JznQ==",
+      "L/S1SF8qA/jV2e9PL5KDxA==",
+      "KX1wZfyvonXHaQb7PREkGw==",
+    ],
   },
 });
